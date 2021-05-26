772c13813d6b93a326a412ab8c53fe9fa7c7308a
Abhirup <abhirup243@gmail.com>
2021-05-26 19:59:11 +0530

    Notes Suitcase Initial Commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>This is a <strong>Session Oriented Tracker</strong></p>
+      
+    </footer>
+  );
+}
+
+export default Footer;
